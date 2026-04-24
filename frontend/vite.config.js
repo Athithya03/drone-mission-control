8c019a25a04c1589b598b522d1497e96e4decb90
Athithya Arunakirinathan <athiarun@Athithyas-MacBook-Air.local>
2026-04-24 06:49:59 +0530

    updated vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // This tells Vite: Anytime the app asks for "/video_feed", 
+      // secretly fetch it from the Raspberry Pi instead.
+      '/video_feed': {
+        target: 'http://172.20.10.2:5000/', // e.g., http://192.168.1.15:8080
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/video_feed/, '')
+      }
+    }
+  }
 })
